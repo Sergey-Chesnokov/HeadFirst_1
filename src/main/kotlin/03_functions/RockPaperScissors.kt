30aeab57055cbfd6c7b49fe1aab3fc7d827df8cb
Sergey <vs-maykop@mail.ru>
2025-01-01 19:28:07 +0300

    RockPaperScissors - Commit03 (finish)

--- src/main/kotlin/03_functions/RockPaperScissors.kt
+++ src/main/kotlin/03_functions/RockPaperScissors.kt
@@ -4,12 +4,11 @@ fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options) // Вызвать функцию getGameChoice
     val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 // Ф-ция getGameChoice - случайным образом выбирает один из вариантов
-fun getGameChoice(optionsParam: Array<String>) {
-    optionsParam[(Math.random() * optionsParam.size).toInt()]
-}
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
 
 // Ф-ция getUserChoice - запрос выбора пользователя
 fun getUserChoice(optionsParam: Array<String>): String {
@@ -36,4 +35,18 @@ fun getUserChoice(optionsParam: Array<String>): String {
         if (!isValidChoice) println("You must enter a valid choice.") // Если ввод пользователя недопустим, цикл продолжается
     }
     return userChoice
+}
+
+fun printResult (userChoice: String, gameChoice: String) {
+    val result: String
+
+    // Определить результат
+    if (userChoice == gameChoice) result = "Tie!" // Ничья
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You lose!"
+
+    // Вывести результат
+    println("You chose $userChoice. I chose $gameChoice. $result")
 }
