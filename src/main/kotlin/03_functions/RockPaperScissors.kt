dce72bbef2c77fe23a9d79a6244c94083826ac10
Sergey <vs-maykop@mail.ru>
2025-01-01 19:09:33 +0300

    RockPaperScissors - Commit02 (add UserChoice)

--- src/main/kotlin/03_functions/RockPaperScissors.kt
+++ src/main/kotlin/03_functions/RockPaperScissors.kt
@@ -3,6 +3,7 @@ package org.example.`03_functions`
 fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options) // Вызвать функцию getGameChoice
+    val userChoice = getUserChoice(options)
 }
 
 // Ф-ция getGameChoice - случайным образом выбирает один из вариантов
@@ -12,11 +13,27 @@ fun getGameChoice(optionsParam: Array<String>) {
 
 // Ф-ция getUserChoice - запрос выбора пользователя
 fun getUserChoice(optionsParam: Array<String>): String {
-    // Запросить у пользователя его выбор
-    println("Please enter one of the following:")
-    for (item in optionsParam) // цикл переберет значение массива options (Rock, Paper, Scissors)
-        print(" $item")
-    println(".")
-    // Прочитать пользовательский ввод
-    val userInput = readLine()
+    var isValidChoice = false // Переменная isValidChoice будет показывать ввел ли пользователь допустимый вариант
+    var userChoice = ""
+
+    // Выполнять цикл, пока пользователь не введет допустимый вариант
+    while (!isValidChoice) { // Цикл продолжиться до тех пор, пока переменная isValidChoice не будет равна true
+
+        // Запросить пользователя его выбор
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+
+        // Прочитать пользовательский ввод
+        val userInput = readLine()
+
+        // Проверить пользовательский ввод
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true // Если ввод пользователя прошел проверку, цикл прерывается
+            userChoice = userInput
+        }
+        // Если выбран недопустимый вариант, сообщить пользователю
+        if (!isValidChoice) println("You must enter a valid choice.") // Если ввод пользователя недопустим, цикл продолжается
+    }
+    return userChoice
 }
